9f0dad917d2f6062793415ed5bf0c80a7af73b8b
Simone Margaritelli <evilsocket@gmail.com>
2021-02-10 02:25:22 +0100

    misc: small fix or general refactoring i did not bother commenting

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,8 +16,9 @@
       <hr class="my-4">
 
       <p style="text-align: center;">
-        The shieldwall agent will instrument your server firewall and block inbound connections from every IP, only
-        allowing the addresses and ports you configured via the backend.
+        The shieldwall agent embraces the zero trust principle and instruments your server firewall to block inbound
+        connections from every IP on any port, by default. The website and API allow you to push policies
+        to your agents and temporarily allow you to reach a certain port from your IP.
       </p>
 
       <div class="container">
@@ -28,7 +29,7 @@
                 Free API
               </div>
               <div class="card-body">
-                <p class="card-text">Create here an account for free and use this server to instrument your agents.</p>
+                <p class="card-text">Create a free account and use this server.</p>
                 <a href="/register" class="btn btn-primary" v-if="!currentUser">Sign Up</a>
                 &nbsp;
                 <a href="/docs" class="btn btn-info" >Quick Start</a>
@@ -42,8 +43,7 @@
                 Host it Yourself
               </div>
               <div class="card-body">
-                <p class="card-text">Host the backend and API service yourself and configure
-                  the agents to communicate in a more discreet setup.</p>
+                <p class="card-text">Host the backend and API service yourself for a more discreet setup.</p>
                 <a href="https://github.com/evilsocket/shieldwall" target="_blank" class="btn btn-success">
                   Code Repository
                 </a>
