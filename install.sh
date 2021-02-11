676e0dd5a7431acfcab0740c2a32bc6617cf3eb4
Simone Margaritelli <evilsocket@gmail.com>
2021-02-11 21:58:06 +0100

    some fixes

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+service shieldwall-agent stop || true
+cp shieldwall-agent /usr/bin/
+mkdir -p /etc/shieldwall/
+test -s /etc/shieldwall/config.yaml || cp agent.example.yaml /etc/shieldwall/config.yaml
+cp shieldwall-agent.service /etc/systemd/system/
+systemctl daemon-reload
